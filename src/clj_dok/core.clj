da01cee2d072b0d21f594a3cf405d5362f3a6182
Fenton Travers <fenton.travers@gmail.com>
2024-09-13 15:28:46 -0700

    .

--- src/clj_dok/core.clj
+++ src/clj_dok/core.clj
@@ -5,6 +5,7 @@
 
 (defn handler [request]
   (let [params (:params request)]
+    ;; spit out Hello, World!
     (response/response (str "Hello, " (or (:name params) "World!") "\n"))))
 
 (def app
@@ -14,6 +15,7 @@
 (def server (atom nil))
 
 (defn -main []
+  ;; Start webserver on port 3000
   (reset! server (jetty/run-jetty app {:port 3000 :join? false}))
   ;; 
   )
